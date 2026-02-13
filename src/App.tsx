273fb3db2cc22df9cddecdaa5f0e6e936277a611
Robyn <scuadra02@gmail.com>
2026-02-13 19:15:06 -0300

    feat: lucide icons implementation
    fix: use alias for imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Button } from "./components/ui/button"
+import { Camera } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button size="lg">Click me</Button>
+      <Camera color="red" size={48} />
     </div>
   )
 }
